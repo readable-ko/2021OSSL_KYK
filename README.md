b724843ecfd83be07d948aa446d78fbba30379cc
midterm <70497430+readable-ko@users.noreply.github.com>
2021-05-02 20:37:22 +0900

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# 2021OSSL_KYK
+![](https://user-images.githubusercontent.com/37644225/116804440-ca55ed00-ab59-11eb-81d2-871317513b8a.jpg)
+
+##### 잃어버린 반려동물 함께 찾아보아요!
+> 서비스 개요 및 기획 의도     
+> 참조 (https://github.com/readable-ko/Miniproject_2/wiki)
+***
+## 협업 도구
+ * Messaging : KaKaotalk
+ * Code Sharing : Github
+
+## 개발 환경 및 언어
+ * C Language
+ * Visual Studio Code
+ 
+## Who are we
+ 1. 고영광([readable-ko](https://github.com/readable-ko))
+ 2. 김태현([drone8449](https://github.com/drone8449))
+
---
+++ main.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <string.h>
+#include "crudfuction.h"
+
+int main() {
+  Animal alist[100];
+  alist[0].age = -1;
+  alist[0].sex = 'K';
+  alist[0].gratuity = -1;
+
+  int count = 0,index =0, menu;
+  int delok;
+
+  //count = loadData(alist);
+  index = count;
+
+  while(1) {
+    menu = selectMenu();
+    if(menu == 0) break;
+    if(menu == 2 || menu == 3 || menu == 4)
+        if(count == 0) continue;
+
+    if(menu == 1) count += createAnimal(&alist[index++]);
+    
+    else if(menu == 2) listAnimal(alist, index);
+    
+    else if(menu == 3) {
+      int num = selectNumber(alist, index);
+      if(num > 0) updateAnimal(&alist[num-1]);
+    }
+
+    else if(menu == 4) {
+      int num = selectNumber(alist, index);
+      if(num > 0) {
+        printf("정말로 삭제하시겠습니까?(삭제 1번) :");
+        scanf("%d",&delok);
+        if(delok == 1) {
+          deleteAnimal(&alist[num-1]);
+          count--;
+        }
+      }
+    }
+
+    else if(menu == 5) {
+      //saveData(alist,index);
+    }
+
+    else if(menu == 6) {
+      printf("\n1. 이름으로 검색\n");
+      printf("2. 품종으로 검색\n");
+      printf("3. 나이로 검색\n");
+      printf("숫자를 입력해주세요 : ");
+      scanf("%d", &delok); /*
+      switch(delok) {
+        case 1: searchName(alist, count); break;
+        case 2: searchType(alist, count); break;
+        case 3: serachAge(alist, count); break;
+        default: break;
+      } */
+
+    }
+  }
+  printf("프로그램이 종료되었습니다.\n");
+  return 0;
+}
